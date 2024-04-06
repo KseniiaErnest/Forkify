9f827fb2358bedd0bd636f54b9fd52d4ba71dda2
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-04-06 09:22:46 -0400

    helpers+config files & event handlers in MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,14 +5,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -28,13 +20,12 @@ const controlRecipe = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-// window.addEventListener('hashchange', controlRecipe);
-// window.addEventListener('load', controlRecipe);
-// But here we duplicate code, so we can do:
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipe)
-);
+const init = function() {
+recipeView.addHandlerRender(controlRecipe);
+};
+
+init();
