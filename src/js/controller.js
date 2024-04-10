6cf8c15e00e42416a7857461aaff880a75d29b6b
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-04-10 11:40:30 -0400

    Uploading Recipe - setting addRecipeView, displaying modal

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import bookmarksView from './views/bookmarksView.js';
 import paginationView from './views/paginationView.js';
+import addRecipeView from './views/addRecipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
@@ -94,6 +95,10 @@ const controlBookmarks = function() {
   bookmarksView.render(model.state.bookmarks);
 }
 
+const controlAddRecipe = function(newRecipe) {
+console.log(newRecipe);
+}
+
 
 ////////////////////////////////////////////////
 const init = function () {
@@ -103,6 +108,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
   
 };
 
